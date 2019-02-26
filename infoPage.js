09fc74ea37d193277b828ac2c870da5eb251e885
Alex Lu Wang <alexluwang2019@u.northwestern.edu>
2019-02-25 21:31:17 -0600

    info page skeleton

---
+++ infoPage.js
@@ -0,0 +1,10 @@
+function changePage(page) {
+    switch(page) {
+        case 'plastics':
+            window.location = ("infoPlastics.html");
+            break;
+        case 'bottles':
+            window.location = ("infoBottles.html");
+            break;
+    }
+}
