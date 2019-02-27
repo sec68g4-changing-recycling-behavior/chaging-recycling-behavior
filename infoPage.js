7db1adfecbe8e826f271c468f927c48a8c40693b
Dave Wang <tianfuwang2021@u.northwestern.edu>
2019-02-26 22:42:15 -0600

    Update infoPage.js

--- infoPage.js
+++ infoPage.js
@@ -6,6 +6,7 @@ function changePage(page) {
         case 'bottles':
             window.location = ("infoBottles.html");
             break;
+
         case 'metals':
             window.location = ("infoMetals.html");
             break;
@@ -21,5 +22,22 @@ function changePage(page) {
         case 'radio':
             window.location = ("infoMetalUn.html");
             break;
+        case 'papers':
+            window.location = ("infoPaper.html");
+            break;
+        case 'cardboard':
+            window.location = ("infoCardboard.html");
+            break;
+        case 'magazine':
+            window.location = ("infoMagazines.html");
+            break;
+        case 'officepaper':
+            window.location = ("infoOfficepaper.html");
+            break;
+        case 'paperother':
+            window.location = ("infoPaperOther.html");
+            break;
+
+
     }
 }
