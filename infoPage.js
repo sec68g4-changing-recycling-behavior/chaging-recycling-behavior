75a23a59fcd9ca838c77dd0fbc6d891d1b0a7954
Dave Wang <tianfuwang2021@u.northwestern.edu>
2019-02-26 22:36:55 -0600

    metals

--- infoPage.js
+++ infoPage.js
@@ -6,5 +6,20 @@ function changePage(page) {
         case 'bottles':
             window.location = ("infoBottles.html");
             break;
+        case 'metals':
+            window.location = ("infoMetals.html");
+            break;
+        case 'metalId':
+            window.location = ("infoMetalId.html");
+            break;
+        case 'cans':
+            window.location = ("infoMetalCans.html");
+            break;
+        case 'scraps':
+            window.location = ("infoMetalScraps.html");
+            break;
+        case 'radio':
+            window.location = ("infoMetalUn.html");
+            break;
     }
 }
