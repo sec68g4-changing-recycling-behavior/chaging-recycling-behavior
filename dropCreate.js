99c7b18592bbe33809ccb0c9158137ab32f44857
ypatelphd <yash.patel@u.northwestern.edu>
2019-03-06 12:05:32 -0600

    added RNM link to all pages, added find drop point to all pages, implemented find drop point, cleaned up create drop point form

--- dropCreate.js
+++ dropCreate.js
@@ -8,3 +8,79 @@ function validateForm() {
 		return false;
 	}
 }
+
+function filterDropPoints() {
+	
+	// Reset all rows and show div
+	document.getElementById("dropPointResults").style.display = "block";
+	materialClick();
+	
+	return false;
+}
+
+function materialClick() {
+	
+	// Ghetto implementation, but it gets the job done!
+	
+	if ((document.getElementById("dropPointResults").style.display == "") ||(document.getElementById("dropPointResults").style.display == "block") ) {
+	
+		// If everything is unchecked, check everything since we never want to show nothing
+		if (!document.getElementById("cbx_metal").checked && !document.getElementById("cbx_paper").checked && !document.getElementById("cbx_plastic").checked && !document.getElementById("cbx_electronics").checked && !document.getElementById("cbx_other").checked) {
+			document.getElementById("cbx_metal").checked = true;
+			document.getElementById("cbx_paper").checked = true;
+			document.getElementById("cbx_plastic").checked = true;
+			document.getElementById("cbx_electronics").checked = true;
+			document.getElementById("cbx_other").checked = true;
+		}
+		
+		// Set bool state value for each material
+		var checkedMetal = false;
+		var checkedPaper = false;
+		var checkedPlastic = false;
+		var checkedElectronics = false;
+		var checkedOther = false;
+		
+		if (document.getElementById("cbx_metal").checked)
+			checkedMetal = true;
+		if (document.getElementById("cbx_paper").checked)
+			checkedPaper = true;
+		if (document.getElementById("cbx_plastic").checked)
+			checkedPlastic = true;
+		if (document.getElementById("cbx_electronics").checked)
+			checkedElectronics = true;
+		if (document.getElementById("cbx_other").checked)
+			checkedOther = true;
+
+		rows = document.getElementsByName("dataRow");
+		var showRow = false;
+
+		for (i=0; i<rows.length; i++) {
+			// If none, show all rows
+			if (checkedMetal || checkedPaper || checkedPlastic || checkedElectronics || checkedOther) {
+				// If something is checked, show the corresponding rows
+				showRow = false;
+				
+				// Note, we're using index of 5 here because the tabs in the source file are being picked up as children of the parent node
+				if (checkedMetal && (rows[i].childNodes[5].innerHTML.indexOf("Metals") != -1))
+					showRow = true;
+				if (checkedPaper && (rows[i].childNodes[5].innerHTML.indexOf("Paper/Cardboard") != -1))
+					showRow = true;
+				if (checkedPlastic && (rows[i].childNodes[5].innerHTML.indexOf("Plastics") != -1))
+					showRow = true;
+				if (checkedElectronics && (rows[i].childNodes[5].innerHTML.indexOf("Electronics") != -1))
+					showRow = true;
+				if (checkedOther && (rows[i].childNodes[5].innerHTML.indexOf("Other") != -1))
+					showRow = true;
+				
+				if (showRow)
+					rows[i].style.display = "";
+				else
+					rows[i].style.display = "none";
+			}
+			else {
+				// Nothing was checked so show all rows
+				rows[i].style.display = "";
+			}
+		}
+	}
+}
